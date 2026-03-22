f05c6989c1d2b23fdf48691d4b43379ecf62b755
Pritam Paul <pritampaulsdev@gmail.com>
2026-03-22 14:29:05 +0530

    utils centralised error handling

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const routerCart = require("./routes/cart");
 
 app.use("/user", routerUser);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/product", routerProduct);
 app.use("/cart", routerCart);
 
 app.listen(3000, () => {
-    console.log("Server is running in port 3000");
-})
+  console.log("Server is running in port 3000");
+});
+
+// FLOW ===>>
+// server -> routes -> controllers -> services -> utils(centralised error handling)
